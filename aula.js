fce953ad94302826f97c79515966cc3d47c321a9
Gabriella Martins <gabmcavlheiro641@gmail.com>
2022-03-11 17:04:52 -0300

    Terminamos o bloco de código 3.3.3.3. Retomar fazendo requisitos junto ao serviço de previsão de tempo

--- aula.js
+++ aula.js
@@ -1,13 +1,108 @@
-const nome = "João"
-const idade = 17
-
-//atalho
-const objeto = { nome, idade }
-
-const objeto2 = {
-    nome: nome,
-    idade: idade
+function calculoRapido (n) {
+    return Promise.resolve((n * (n + 1)) /2)
 }
+
+function f (n) {
+    return new Promise((resolve, reject) => {
+        resolve((n * (n + 1)) /2)
+    }) 
+}
+
+calculoRapido(10).then (res => console.log(res))
+f(10).then (res => console.log(res))
+
+//1 + 2 + ... + n-1 + n
+// function calculoDemorado (n) { //maneira assincrona
+//     let p = new Promise((resolve, reject) => {
+//         if (n < 0) {
+//             reject('Somente valores positivos')
+//         }
+//         else {
+//             let res = 0
+//             for (let i = 1; i <= n; i++)
+//                 res += i
+//             resolve(res)
+//         }
+            
+//     })
+//     return p
+// }
+
+// calculoDemorado(-10)
+// .then ((res) => {
+//     console.log("Ok deu certo: " + res)
+// })
+// .catch (erro => {
+//     console.log("Falhou: " + erro)
+// })
+
+// const resultadoFuturo = calculoDemorado(10)
+// resultadoFuturo.then((res) => console.log(res))
+
+
+// function exemplo(){
+//     //função associada a computação(sequencia de passos? não me lembro ao certo) dessa promise
+//     let p = new Promise(function(resolve, reject){ //resolve = sucesso e reject = possivel erro
+//         resolve(1)
+
+//     })
+//     return p 
+// }
+
+// const resultadoFuturo = exemplo()
+// resultadoFuturo.then((resultado) => console.log(resultado))
+// console.log(resultadoFuturo)
+
+// function exemplo () {
+//     return 1
+// }
+
+// const resultado = exemplo()
+// console.log(resultado)
+
+
+// const fs = require('fs')
+// const abrirArquivo = function (nomeArquivo) {
+//     const exibirConteudo = function (erro, conteudo) {
+//         if (erro) {
+//             console.log("Deu erro: " + erro)
+//         }
+//         else {
+//             console.log(conteudo.toString())
+//             //processamento
+//             const dobro = 2 * +conteudo.toString() //+conteudo -> conversao de texto para numero
+
+//             const finalizar = function (erro) {
+//                 if (erro) {
+//                     console.log("Deu erro: " + erro)
+//                 }
+//                 else {
+//                     console.log("Salvou o dobro com sucesso")
+//                 }
+//             }
+//             fs.writeFile('dobro.txt', dobro.toString(), finalizar) //writeFile IO Bound
+//         }
+//     }
+//     //segundo parâmetro: função callback 
+//     fs.readFile(nomeArquivo, exibirConteudo)
+// }
+
+// abrirArquivo('arquivo.txt')
+
+// IO Bound -> entrada e saida de dados
+// CPU Bound -> caracterizado pelo uso da cpu
+
+
+// const nome = "João"
+// const idade = 17
+
+// //atalho
+// const objeto = { nome, idade }
+
+// const objeto2 = {
+//     nome: nome,
+//     idade: idade
+// }
 
 
 // let calc = {
